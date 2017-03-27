d506de04cd5a33ad4071e6fc62270eda6cbd97d4
minhochoi <mchoi@hanekedesign.com>
2017-03-27 15:11:18 -0400

    Setup all development environment

--- single.php
+++ single.php
@@ -1,1 +1,15 @@
 <?php get_template_part('templates/content-single', get_post_type()); ?>
+
+<!-- <?php
+if (have_posts()) : while (have_posts()) : the_post();
+
+if( is_category( 'Portfolio' ) ):
+get_template_part( 'single','portfolio' );
+else if ( is_category( 'News' ) ):
+get_template_part( 'single','news' );
+else( is_category( 'parameter' ) ):
+get_template_part('templates/content', 'single');
+endwhile;
+?>
+
+ -->
